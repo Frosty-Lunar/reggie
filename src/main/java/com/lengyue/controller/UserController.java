6b41401b6870ce705d29340664da5c191471b926
lengyue <201628142@qq.com>
2023-01-12 22:07:22 +0800

    redis缓存手机验证码

--- src/main/java/com/lengyue/controller/UserController.java
+++ src/main/java/com/lengyue/controller/UserController.java
@@ -10,6 +10,7 @@ import com.lengyue.utils.ValidateCodeUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
@@ -24,6 +26,8 @@ import java.util.Map;
 public class UserController {
     @Autowired
     private UserService userService;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @PostMapping("/login")
     public Result<Object> login(@RequestBody Map map, HttpSession httpSession) {
@@ -34,9 +38,9 @@ public class UserController {
         //获取验证码
         String code = map.get("code").toString();
         //从Session中获取保存的验证码
-        Object codeInSession = httpSession.getAttribute(phone);
+        Object codeInRedis = redisTemplate.opsForValue().get(phone);
         //进行验证码的比对（页面提交的验证码和Session中保存的验证码比对）
-        if (codeInSession != null && codeInSession.equals(code)) {
+        if (codeInRedis != null && codeInRedis.equals(code)) {
             //如果能够比对成功，说明登录成功
             LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
             queryWrapper.eq(User::getPhone, phone);
@@ -50,6 +54,7 @@ public class UserController {
                 userService.save(user);
             }
             httpSession.setAttribute("user", user.getId());
+            redisTemplate.delete(phone);
             return Result.success(user);
         }
         return Result.error("登录失败");
@@ -61,9 +66,9 @@ public class UserController {
         if (StringUtils.isNotEmpty(user.getPhone())) {
             String code = ValidateCodeUtils.generateValidateCode(4).toString();
             log.info("验证码：{}", code);
-            httpSession.setAttribute(user.getPhone(), code);
 /*            SMSUtils.sendMessage("瑞吉外卖",
                     "SMS_266905319", user.getPhone().toString(), code);*/
+            redisTemplate.opsForValue().set(user.getPhone(), code, 5, TimeUnit.MINUTES);
             return Result.success("验证码发送成功！");
         }
         return Result.error("请重新输入手机号！");
